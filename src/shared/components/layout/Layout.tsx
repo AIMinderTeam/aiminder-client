83634e91fc0fc1a40bee880fce2eb8538d4103bd
injulme <injulme0309@naver.com>
2025-08-05 16:02:45 +0900

    feat: Layout 컴포넌트 분리

--- src/shared/components/layout/Layout.tsx
+++ src/shared/components/layout/Layout.tsx
@@ -1,15 +1,48 @@
 import styles from './Layout.module.css';
 
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
-import { Bell } from 'lucide-react';
+import { Bell, ChevronLeft, EllipsisVertical } from 'lucide-react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
+  console.log(pathname);
+  const isHome = pathname === '/';
+  const isNotification = pathname === '/notification';
+
+  const goBack = () => {
+    navigate(-1);
+  };
 
   return (
     <main>
       <header className={styles.LayoutHeader}>
+        {isHome ? <HomeHeader /> : isNotification ? <NotificationHeader /> : null}
+      </header>
+      {children}
+      {isNotification ? null : (
+        <nav className={styles.LayoutNav}>
+          <Link to="/" className={styles.LayoutNavLink}>
+            Home
+          </Link>
+          <Link to="/goal" className={styles.LayoutNavLink}>
+            Goal
+          </Link>
+          <Link to="/ai-assistant" className={styles.LayoutNavLink}>
+            Ai Assistant
+          </Link>
+          <Link to="/login" className={styles.LayoutNavLink}>
+            Login
+          </Link>
+        </nav>
+      )}
+    </main>
+  );
+
+  function HomeHeader() {
+    return (
+      <>
         <h1 className={styles.LayoutHeaderTitle} onClick={() => navigate('/')}>
           AIMinder
         </h1>
@@ -17,22 +50,21 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <Bell size={24} className={styles.LayoutBellIcon} />
           <span className={styles.LayoutBellDot} />
         </Link>
-      </header>
-      {children}
-      <nav className={styles.LayoutNav}>
-        <Link to="/" className={styles.LayoutNavLink}>
-          Home
-        </Link>
-        <Link to="/goal" className={styles.LayoutNavLink}>
-          Goal
-        </Link>
-        <Link to="/ai-assistant" className={styles.LayoutNavLink}>
-          Ai Assistant
-        </Link>
-        <Link to="/login" className={styles.LayoutNavLink}>
-          Login
-        </Link>
-      </nav>
-    </main>
-  );
+      </>
+    );
+  }
+
+  function NotificationHeader() {
+    return (
+      <>
+        <button onClick={goBack} className={styles.LayoutHeaderButtonContainer}>
+          <ChevronLeft size={24} />
+        </button>
+        <h1 className={styles.LayoutSubHeaderTitle}>알림</h1>
+        <button onClick={goBack} className={styles.LayoutHeaderButtonContainer}>
+          <EllipsisVertical size={24} />
+        </button>
+      </>
+    );
+  }
 }
