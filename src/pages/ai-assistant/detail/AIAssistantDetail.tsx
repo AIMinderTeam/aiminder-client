29d229e36a02c4d2f811838b68415982c5646422
injulme <injulme0309@naver.com>
2025-08-06 18:47:22 +0900

    feat: Ai Assistant Detail Chatting Input UI 구현

--- src/pages/ai-assistant/detail/AIAssistantDetail.tsx
+++ src/pages/ai-assistant/detail/AIAssistantDetail.tsx
@@ -1,9 +1,21 @@
-import styles from './AIAssistantDetail.module.css';
+import styles from './AiAssistantDetail.module.css';
 
 import { useParams } from 'react-router-dom';
+
+import { SendHorizonal } from 'lucide-react';
 
 export default function AIAssistantDetail() {
   const { aiAssistantId } = useParams();
 
-  return <section className={styles.AiAssistantDetailSection}>ai assistant detail :: {aiAssistantId}</section>;
+  return (
+    <section className={styles.AiAssistantDetailSection}>
+      ai assistant detail :: {aiAssistantId}
+      <div className={styles.AiAssistantDetailInputContainer}>
+        <input type="text" placeholder="메시지를 입력해주세요." className={styles.AiAssistantDetailInput} />
+        <button className={styles.AiAssistantDetailInputSendButton}>
+          <SendHorizonal size={20} strokeWidth={1.5} />
+        </button>
+      </div>
+    </section>
+  );
 }
