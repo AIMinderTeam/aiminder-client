a6b645c37ef04c528307f36fb1052841ab3cd0cb
injulme <injulme0309@naver.com>
2025-08-13 22:26:20 +0900

    feat: Daily Goal UI 구현

--- src/pages/daily-goal/DailyGoal.tsx
+++ src/pages/daily-goal/DailyGoal.tsx
@@ -7,5 +7,16 @@ export default function DailyGoal() {
 
   console.log(dailyGoalId);
 
-  return <section className={styles.DailyGoalSection}>DailyGoal: {dailyGoalId}</section>;
+  return (
+    <section className={styles.DailyGoalSection}>
+      <div className={styles.DailyGoalTitleContainer}>
+        <div className={styles.DailyGoalTitleContainerHeader}>
+          <span className={styles.StatusBadge}>진행</span>
+          <span>2025-08-13</span>
+        </div>
+        <span className={styles.DailyGoalTitleContainerTitle}>유산소 30분 (걷기/러닝)</span>
+      </div>
+      <div className={styles.DailyGoalMemoContainer}>메모</div>
+    </section>
+  );
 }
