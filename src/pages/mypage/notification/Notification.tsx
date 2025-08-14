88d29a542d9b1ccb922110756dbb6ee2e426c1d5
injulme <injulme0309@naver.com>
2025-08-14 21:36:53 +0900

    feat: 마이페이지 알림 및 후기 UI 구현

--- src/pages/mypage/notification/Notification.tsx
+++ src/pages/mypage/notification/Notification.tsx
@@ -1,3 +1,22 @@
+import styles from './Notification.module.css';
+
+import Switch from '@/shared/components/ui/switch';
+
 export default function Notification() {
-  return <div>Notification</div>;
+  return (
+    <section className={styles.NotificationSection}>
+      <div className={styles.NotificationItem}>
+        전체 알림 <Switch />
+      </div>
+      <div className={styles.NotificationItem}>
+        오늘의 할 일 알림 <Switch />
+      </div>
+      <div className={styles.NotificationItem}>
+        오늘의 동기부여 알림 <Switch />
+      </div>
+      <div className={styles.NotificationItem}>
+        AI 비서 피드백 알림 <Switch />
+      </div>
+    </section>
+  );
 }
--- src/pages/mypage/review/Review.tsx
+++ src/pages/mypage/review/Review.tsx
@@ -1,3 +1,12 @@
+import styles from './Review.module.css';
+
+import { Button } from '@/shared/components/ui/button';
+
 export default function Review() {
-  return <div>Review</div>;
+  return (
+    <section className={styles.ReviewSection}>
+      <textarea name="content" placeholder="내용을 입력해주세요." className={styles.ReviewTextarea}></textarea>
+      <Button className={styles.ReviewButton}>등록</Button>
+    </section>
+  );
 }
