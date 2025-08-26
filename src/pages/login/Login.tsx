0c83f993a88b9e01b136eb3ccc6ae8658fc5f375
injulme <injulme0309@naver.com>
2025-08-26 21:12:28 +0900

    chore: Login 버튼 클릭 시 home으로 이동

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,6 @@
 import styles from './Login.module.css';
 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import googleIcon from '@/assets/google.png';
 import kakaoIcon from '@/assets/kakao.png';
@@ -8,6 +8,10 @@ import { Button } from '@/shared/components/ui/button';
 import { Bot } from 'lucide-react';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate('/');
+  };
   return (
     <section className={styles.LoginSection}>
       <div className={styles.LoginLogoContainer}>
@@ -17,11 +21,11 @@ export default function Login() {
 
       <div className={styles.LoginButtonContainer}>
         <div className={styles.LoginDescription}>AI와 함께 나의 루틴 계획을 공유해보세요!</div>
-        <Button className={styles.LoginButton}>
+        <Button className={styles.LoginButton} onClick={goHome}>
           <img src={googleIcon} alt="google" className={styles.GoogleIcon} />
           <span className={styles.LoginButtonText}>구글 로그인</span>
         </Button>
-        <Button className={styles.LoginButton}>
+        <Button className={styles.LoginButton} onClick={goHome}>
           <img src={kakaoIcon} alt="kakao" className={styles.KakaoIcon} />
           <span className={styles.LoginButtonText}>카카오 로그인</span>
         </Button>
