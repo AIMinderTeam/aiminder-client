6d9bb028ff0e3fe40becd9fe3bc56845cb0e5b5d
injulme <injulme0309@naver.com>
2025-09-02 11:24:10 +0900

    feat: 내비게이션 링크 활성화 스타일 추가 및 경로에 따른 클래스 적용
    - 내비게이션 링크에 활성화 상태에 따른 스타일 추가
    - useLocation 훅을 사용하여 현재 경로에 따라 링크의 활성화 상태 결정
    - CSS 모듈에 새로운 활성화 클래스 정의

--- src/shared/components/layout/navigation.tsx
+++ src/shared/components/layout/navigation.tsx
@@ -1,23 +1,43 @@
 import styles from './navigation.module.css';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
+import classNames from 'classnames';
 
 export default function Navigation() {
+  const { pathname } = useLocation();
+
+  const isActive = (path: string) => {
+    if (path === '/') {
+      return pathname === path;
+    } else {
+      return pathname.includes(path);
+    }
+  };
   return (
     <nav className={styles.Navigation}>
-      <Link to="/" className={styles.NavigationLink}>
+      <Link to="/" className={classNames(styles.NavigationLink, isActive('/') && styles.NavigationLinkActive)}>
         Home
       </Link>
-      <Link to="/goal" className={styles.NavigationLink}>
+      <Link to="/goal" className={classNames(styles.NavigationLink, isActive('/goal') && styles.NavigationLinkActive)}>
         Goal
       </Link>
-      <Link to="/ai-assistant" className={styles.NavigationLink}>
+      <Link
+        to="/ai-assistant"
+        className={classNames(styles.NavigationLink, isActive('/ai-assistant') && styles.NavigationLinkActive)}
+      >
         Ai Assistant
       </Link>
-      <Link to="/login" className={styles.NavigationLink}>
+      <Link
+        to="/login"
+        className={classNames(styles.NavigationLink, isActive('/login') && styles.NavigationLinkActive)}
+      >
         Login
       </Link>
-      <Link to="/mypage" className={styles.NavigationLink}>
+      <Link
+        to="/mypage"
+        className={classNames(styles.NavigationLink, isActive('/mypage') && styles.NavigationLinkActive)}
+      >
         Mypage
       </Link>
     </nav>
